f24f4380b16d15c22b3583b736d69b7828b978fb
SabalKatuwal <sabal.katuwal1@gmail.com>
2026-06-12 18:52:26 +0545

    Initial Commit

---
+++ FourteenZoneTask/FourteenZoneTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourteenZoneTaskApp.swift
+//  FourteenZoneTask
+//
+//  Created by Sabal on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct FourteenZoneTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
